ed1ef4eff1c2418f94e50fa840a5a4e49d42b98b
Beata Nemeth <beataspace@gmail.com>
2026-05-12 22:24:27 +0200

    chore: set up static site generation; remove unused images from public folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
+  basePath: isProd ? '/portfolio' : '',
+  // Use unoptimized images for GitHub Pages static export
+  // This is the simplest way to avoid needing a custom loader file
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
